523f4b45aa0a9df1cc34cfc58e2a7d683e14dbee
Subho Halder <subho.halder@gmail.com>
2017-10-02 04:18:18 +0530

    Add keymap for history search

--- zshconf/conf.zsh
+++ zshconf/conf.zsh
@@ -53,6 +53,12 @@ export ZSH_PLUGINS_ALIAS_TIPS_TEXT=' alias hint: '
 
 zstyle ':prezto:module:terminal' auto-title 'yes'
 
+if zplug check zsh-users/zsh-history-substring-search; then
+  zmodload zsh/terminfo
+  bindkey "$terminfo[cuu1]" history-substring-search-up
+  bindkey "$terminfo[cud1]" history-substring-search-down
+fi
+
 # use the custom history modules
 source $HOME/.dotfiles/zshconf/history.zsh
 
