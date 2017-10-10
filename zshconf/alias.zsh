8a64c736b8202136e065a10fe238e09212a18eb8
Subho Halder <subho.halder@gmail.com>
2017-10-11 01:11:59 +0530

    book mark alias which I'll remember

--- zshconf/alias.zsh
+++ zshconf/alias.zsh
@@ -1,10 +1,10 @@
 alias l="ls"  # normal ls
 alias ll="ls -al"
 alias kk="k -a"  # zsh plugin for k
-alias g="jump"
-alias s="bookmark"
-alias d="deletemark"
-alias p="showmarks"
+alias j="jump"
+alias b="bookmark"
+alias dm="deletemark"
+alias lb="showmarks"
 
 # Reload the shell (i.e. invoke as a login shell)
 alias reload="exec $SHELL -l"
